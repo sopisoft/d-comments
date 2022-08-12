55f47278df0b9ca81dbc53f89f356926a8c27528
牛蒡 <gobosan@outlook.jp>
2022-08-12 17:03:53 +0900

    License

--- src/Background/index.ts
+++ src/Background/index.ts
@@ -1,9 +1,20 @@
-/**
- * 任意の範囲の整数を返す
- * @param min 最小値
- * @param max 最大値
- * @returns number between min and max
- */
+/*
+    This file is part of d-comments.
+
+    d-comments is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    d-comments is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with d-comments.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 const getRandomInt = (min: number, max: number) => {
   min = Math.ceil(min);
   max = Math.floor(max);
