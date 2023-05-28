4f02d1a81500c7a140cbcbeae9ae3732d056af67
gobosan <gobosan@outlook.jp>
2023-05-28 17:36:35 +0900

    fix #33

--- src/content_scripts/migrate.ts
+++ src/content_scripts/migrate.ts
@@ -1,12 +1,12 @@
 import { getConfig, setConfig } from "./config";
 
 export const migrate = () => {
-	getConfig("way_to_render_comments", (way) => {
-		if (!way) {
-			getConfig("flow_comments", (val) => {
-				if (val) {
+	chrome.storage.local.get(["way_to_render_comments"], (way) => {
+		if (!way["way_to_render_comments"]) {
+			chrome.storage.local.get(["flow_comments"], (result) => {
+				if (result["flow_comments"]) {
 					setConfig("way_to_render_comments", "right_to_left");
-				} else if (val === false) {
+				} else if (result["flow_comments"] === false) {
 					setConfig("way_to_render_comments", "list");
 				}
 			});
