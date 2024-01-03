e904289fd44db02b9949dfdc0ce4729e8a64c457
gobosan <gobosan@outlook.jp>
2024-01-03 19:13:37 +0900

    webExt build

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,3 +1,4 @@
+import fs from "fs";
 import Bun from "bun";
 import { build } from "vite";
 import { manifest } from "./manifest";
@@ -6,13 +7,24 @@ import { webExtBuild } from "./web-ext";
 
 const browsers: browsers = ["chrome", "firefox"];
 
-browsers.map((browser) =>
+process.env.NODE_ENV = "production";
+
+Promise.all([
+  fs.rmdirSync("dist", { recursive: true }),
+  Bun.spawn(["bunx", "--bun", "tsc"]),
+]);
+
+const builds = browsers.map((browser) =>
   build({
     mode: browser,
     build: {
       outDir: `../dist/${browser}`,
     },
-  }).then(() => {
+  })
+);
+
+Promise.all(builds).then(() => {
+  browsers.map((browser) => {
     Promise.all([
       manifest(browser).then((manifest) => {
         Bun.write(
@@ -25,5 +37,5 @@ browsers.map((browser) =>
         webExtBuild(browser);
       }),
     ]);
-  })
-);
+  });
+});
--- scripts/web-ext.ts
+++ scripts/web-ext.ts
@@ -1,31 +1,14 @@
-// import webExt from "web-ext";
-import fs from "fs";
-import path, { resolve } from "path";
-import Bun from "bun";
+// @ts-ignore
+import webExt from "web-ext";
 
 export async function webExtBuild(browser: browsers[number]) {
   const sourceDir = `dist/${browser}/`;
-  // webExt.cmd.build({
-  //   sourceDir: sourceDir,
-  //   overwriteDest: true,
-  //   filename: browser,
-  // });
-
-  Bun.spawn(
-    ["bunx", "web-ext", "build", "--source-dir", sourceDir, "--overwrite-dest"],
-    {
-      onExit: () => {
-        const folder = path.join(process.cwd(), "web-ext-artifacts");
-        const files = fs.readdirSync(folder).map((file) => {
-          return path.join(folder, file);
-        });
-        const zip = files.find((file) => {
-          return file.endsWith(".zip");
-        });
-        const new_path = resolve(`${process.cwd()}/dist/`, `${browser}.zip`);
-        zip && fs.renameSync(zip, new_path);
-        fs.rmdirSync("web-ext-artifacts", { recursive: true });
-      },
-    }
-  );
+  webExt.cmd.build({
+    sourceDir: sourceDir,
+    artifactsDir: "dist",
+    asNeeded: false,
+    overwriteDest: true,
+    ignoreFiles: [],
+    filename: `${browser}.zip`,
+  });
 }
